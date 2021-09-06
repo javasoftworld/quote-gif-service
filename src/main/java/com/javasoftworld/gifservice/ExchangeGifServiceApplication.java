74e343582a33e25e7d14dab4455a017ea4065418
Sergei Belonosov <2belonosov@gmail.com>
2021-09-06 13:49:29 +0700

    Initial commit

---
+++ src/main/java/com/javasoftworld/gifservice/ExchangeGifServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javasoftworld.gifservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeGifServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeGifServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javasoftworld/gifservice/ExchangeGifServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasoftworld.gifservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeGifServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
